081dbc0c2dc7e65a388b5fb38f814aac1974c2be
Gabriel.Almeida-10 <105819653+space-m0nkey@users.noreply.github.com>
2024-09-18 15:41:55 -0300

    Update content.js
    Signed-off-by: Gabriel.Almeida-10 <105819653+space-m0nkey@users.noreply.github.com>

--- crm-react/content.js
+++ crm-react/content.js
@@ -3,12 +3,30 @@ console.log("JavaScript injetado em uma página!");
 
 (function() {
   const newElement = document.createElement('div');
-  newElement.innerText = 'Olá, esta é uma injeção de JavaScript!!!';
+  newElement.innerText = 'JavaScript injetado!!!';
   newElement.style.position = 'fixed';
   newElement.style.bottom = '10px';
   newElement.style.right = '10px';
-  newElement.style.backgroundColor = 'lightblue';
+  newElement.style.backgroundColor = 'red';
   newElement.style.padding = '10px';
   newElement.style.zIndex = '9999';
   document.body.appendChild(newElement);
 })();
+function minhaFuncao() {
+
+  const divElement = document.querySelector('.q-item__label.text-bold');
+  if (divElement) {
+
+      const content = divElement.textContent.trim();
+      console.log(content); 
+      alert("Funcionou: " + content); 
+
+  } else {
+
+      console.log('Elemento não encontrado');
+      alert("Não funcionou");
+  }
+}
+setTimeout(minhaFuncao, 3000);
+
+//08/09/2024#836281arrow_drop_down
