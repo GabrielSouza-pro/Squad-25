1874eb7a4b928f5184f79cba7fda044f9f0f60b2
Gabriel.Almeida-10 <105819653+space-m0nkey@users.noreply.github.com>
2024-11-06 22:20:18 -0300

    Update background.js
    Signed-off-by: Gabriel.Almeida-10 <105819653+space-m0nkey@users.noreply.github.com>

--- crm-prod/dist/background.js
+++ crm-prod/dist/background.js
@@ -1,10 +1,16 @@
+//background.js
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
-  if (request.action === 'fetchData') {
+  if (request.action === 'fetchData' || request.action === 'fetchDeals' || request.action === 'fetchDealProducts') {
     fetch(request.url, {
       method: 'GET',
       headers: request.headers
     })
-    .then(response => response.json())
+    .then(response => {
+      if (!response.ok) {
+        throw new Error(`Erro na requisição: ${response.statusText}`);
+      }
+      return response.json();
+    })
     .then(data => sendResponse({ success: true, data }))
     .catch(error => sendResponse({ success: false, error: error.message }));
     return true; // Indica que a resposta será enviada de forma assíncrona
